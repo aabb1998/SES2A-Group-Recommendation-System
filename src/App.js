39f1ba25a5e879e3c4fe4febb93ddaa0dc3c1fee
aabb1998 <53510613+aabb1998@users.noreply.github.com>
2020-08-19 17:14:08 +1000

    Implemented Firebase
    Implemented Firebase and MaterialUI functionality.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>
-            test 
-          </h1>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is the SES 2A Group-Recommendation-System</h1>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyALyKUb0HuB2d2u0JHZRU_B6QZ44BCyS3I",
+  authDomain: "group-recommendation-sys-15e25.firebaseapp.com",
+  databaseURL: "https://group-recommendation-sys-15e25.firebaseio.com",
+  projectId: "group-recommendation-sys-15e25",
+  storageBucket: "group-recommendation-sys-15e25.appspot.com",
+  messagingSenderId: "121869203714",
+  appId: "1:121869203714:web:a74176761542b7ca006a1b",
+  measurementId: "G-LLD7KM3TSN",
+};
